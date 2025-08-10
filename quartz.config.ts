7a3842054afabf9cf13e230e67b8d5f526a408e9
diraw <craftyup7567@outlook.com>
2025-08-10 13:54:41 +0800

    Quartz sync: Aug 10, 2025, 1:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "",
+    baseUrl: "/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
